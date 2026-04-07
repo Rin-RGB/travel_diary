1f48d5f2b025f421ad0843e4a0f23173ffc531dd
hsugvusrihgvir <dariasugarrrrr@gmail.com>
2026-04-07 19:24:21 +0300

    main cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,10 @@ app = FastAPI(
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://127.0.0.1:63343",
+    "http://localhost:63343",
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
