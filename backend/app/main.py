cacb30b4f59d0528fc6ae34154986a5e704fc667
hsugvusrihgvir <dariasugarrrrr@gmail.com>
2026-04-07 18:54:33 +0300

    main cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
-from sqlalchemy.testing.suite.test_reflection import users
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth, health, places, folders, cities, users, place_statuses, tags
+
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Travel Places API",
     version="1.0.0"
+)
+
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(health.router)
@@ -16,3 +31,5 @@ app.include_router(cities.router)
 app.include_router(users.router)
 app.include_router(place_statuses.router)
 app.include_router(tags.router)
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
